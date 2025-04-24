08b399d10cd9bf5d59ee322994a9296e579ec8af
danechka2228 <d97944535@gmail.com>
2025-04-24 19:23:14 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import *
-import pygame # импортируем библеотеку ыыыыы
+import pygame # импортируем библеотеку ыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыы
 class Food(): # создание класса
     def __init__(self, a, c, d): # создание конструктора в нем создаются свойства , вызывается при создании объекта
         self.image = pygame.image.load(a)# загружаем картинку, это свойство
@@ -10,17 +10,19 @@ class Food(): # создание класса
     def move_plate(self): #
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] == True:
-             self.rect.x -= 5 # движение объекта
+             self.rect.x -= 8 # движение объекта
         if keys[pygame.K_RIGHT]:
-            self.rect.x += 5  # движение объекта
+            self.rect.x += 8  # движение объекта
 
     def move_food(self):
         self.rect.y += 5
         if self.rect.y > 700:
-            self.rect.y = 0
+            self.rect.y = -100
 
     def draw_image(self):  #метод отрисовки картинки
         screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
 
 
 
@@ -35,11 +37,14 @@ f1 = Food('pelmen.png', 50,  t)#создание объекта класса foo
 f2 = Food('cheesburger.png', 300, q )#создание объекта класса food
 f3 = Food('pelmen.png', 500, g )#создание объекта класса food
 f4 = Food('gummy bear.png', 700, f )#создние объекта класса food
+f5 = Food('gummy bear.png', 800, h )#создние объекта класса food
 plate = Food('plate.png', 380, 600)#создание объекта класса food
+food_list = [f1 , f2 , f3 , f4, f5 ]
 
 
 
-pygame.init() #обязательная программа
+
+pygame.init() #обязательная функция
 window_size=(1000 , 700) #размеры окна
 screen=pygame.display.set_mode(window_size) #создаем экран с размерами
 pygame.display.set_caption("Doom eternal") #название игры
@@ -49,20 +54,22 @@ while True:# игровой цикл
 
     fon.draw_image()#применение метода отрисовки картинки
     plate.move_plate()#применение метода отрисовки картинки
-    f1.draw_image()#применение метода отрисовки картинки
-    f2.draw_image()#применение метода отрисовки картинки
-    f3.draw_image()#применение метода отрисовки картинки
-    f4.draw_image()#применение метода отрисовки картинки
     plate.draw_image()#применение метода отрисовки картинки
     clock.tick(65)# фпс
+    for i in food_list:
+        i.draw_image()
+        i.move_food()
+        if plate.rect.colliderect(i.rect):
+            food_list.remove(i)
+    if food_list == [ ]:
+        pygame.exit()
     for event in pygame.event.get(): # ходим по событиям
         if event.type == pygame.QUIT: # нажали на крестик
             pygame.QUIT() # вышли из игры
-    f1.move_food()#применение метода отрисовки картинки
-    f2.move_food()#применение метода отрисовки картинки
-    f3.move_food()#применение метода отрисовки картинки
-    f4.move_food()#применение метода отрисовки картинки
     pygame.display.update() # обнова экрана
+
+
+
 
 
 
